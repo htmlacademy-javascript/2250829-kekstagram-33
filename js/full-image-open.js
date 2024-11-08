2d3ace2b4152f022e5b25439a16d47434083752a
Кирилл Леухин <kirvtoroj7@gmail.com>
2024-11-08 20:13:31 +0500

    Реализует изменение количества показываемых комментариев

--- js/full-image-open.js
+++ js/full-image-open.js
@@ -1,11 +1,26 @@
 import { isEscapeKey } from './util.js';
-import { renderFullImage, socialCommentsList } from './full-image-render.js';
+import { socialCommentsList, socialCommentTotalCount, renderComments } from './render-comments.js';
+import { renderFullImage } from './full-image-render.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
 const pictureLinks = document.querySelectorAll('.picture');
-const socialCommentCount = document.querySelector('.social__comment-count');
+const socialCommentShownCount = document.querySelector('.social__comment-shown-count');
+let socialCommentShownCountNumber = Number(socialCommentShownCount.textContent);
 const commentsLoader = document.querySelector('.comments-loader');
+let bigImageComments = [];
+
+const onCommentsLoaderClick = () => {
+  socialCommentsList.innerHTML = '';
+  socialCommentShownCountNumber += 5;
+  socialCommentShownCount.textContent = socialCommentShownCountNumber;
+
+  if (socialCommentShownCountNumber >= Number(socialCommentTotalCount.textContent)) {
+    socialCommentShownCount.textContent = socialCommentTotalCount.textContent;
+    commentsLoader.classList.add('hidden');
+  }
+  renderComments(bigImageComments, Number(socialCommentShownCount.textContent));
+};
 
 const onBigPictureKeyDown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -16,11 +31,22 @@ const onBigPictureKeyDown = (evt) => {
 
 const openImageModal = (evt) => {
   bigPicture.classList.remove('hidden');
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   document.body.classList.add('modal-open');
+  bigImageComments = evt.currentTarget.myComments;
+  socialCommentTotalCount.textContent = bigImageComments.length;
   socialCommentsList.innerHTML = '';
+  socialCommentShownCount.textContent = 5;
+  socialCommentShownCountNumber = 5;
+
+  commentsLoader.classList.remove('hidden');
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
   renderFullImage(evt);
+  if (Number(socialCommentTotalCount.textContent) <= 5) {
+    socialCommentShownCount.textContent = socialCommentTotalCount.textContent;
+    commentsLoader.classList.add('hidden');
+  }
+  renderComments(bigImageComments, Number(socialCommentShownCount.textContent));
 
   document.addEventListener('keydown', onBigPictureKeyDown);
 };
@@ -29,6 +55,7 @@ const closeImageModal = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.add('modal-open');
 
+  commentsLoader.removeEventListener('click', onCommentsLoaderClick);
   document.removeEventListener('keydown', onBigPictureKeyDown);
 };
 
@@ -44,4 +71,3 @@ bigPictureCancel.addEventListener('click', (evt) => {
   evt.preventDefault();
   closeImageModal();
 });
-
