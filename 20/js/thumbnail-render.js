3b3a73c6747981bfbe9340840e95bd4eb4d72602
Keks <keks@htmlacademy.ru>
2024-11-17 16:32:28 +0300

    :heavy_check_mark: Сборка #20

---
+++ 20/js/thumbnail-render.js
@@ -0,0 +1,31 @@
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content;
+const pictureLink = pictureTemplate.querySelector('.picture');
+const imagesFragment = document.createDocumentFragment();
+
+
+const createImage = (picture, pictureClickHandler) => {
+  const pictureLinkCloned = pictureLink.cloneNode(true);
+  const pictureImg = pictureLinkCloned.querySelector('.picture__img');
+  pictureImg.src = picture.url;
+  pictureImg.alt = picture.description;
+  const pictureInfo = pictureLinkCloned.querySelector('.picture__info');
+  pictureInfo.querySelector('.picture__likes').textContent = picture.likes;
+  pictureInfo.querySelector('.picture__comments').textContent = picture.comments.length;
+
+  pictureLinkCloned.addEventListener('click', () => {
+    pictureClickHandler(picture);
+  });
+
+  return pictureLinkCloned;
+};
+
+const renderImage = (photosData, pictureClickHandler) => {
+  photosData.forEach((picture) => {
+    const newImage = createImage(picture, pictureClickHandler);
+    imagesFragment.append(newImage);
+  });
+  picturesContainer.append(imagesFragment);
+};
+
+export { renderImage };
