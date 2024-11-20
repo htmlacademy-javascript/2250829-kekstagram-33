b62dd93e9675f06798e0ffbd72bf3e3c6bb706e3
Кирилл Леухин <kirvtoroj7@gmail.com>
2024-11-20 19:07:05 +0500

    Рефакторит очистку формы

--- js/reset-image-form.js
+++ js/reset-image-form.js
@@ -1,16 +1,10 @@
-import { scaleValue, SCALE_VALUE_MAX_NUMBER, imagePreview, changeImageScale } from './image-scale.js';
+import { SCALE_VALUE_MAX_NUMBER, imagePreview, changeImageScale } from './image-scale.js';
 
-const imageComment = document.querySelector('.text__description');
-const imageHashtags = document.querySelector('.text__hashtags');
-const imageUploadInput = document.querySelector('.img-upload__input');
 const effectSliderContainer = document.querySelector('.effect-level__slider');
 const effectOriginal = document.querySelector('#effect-none');
 
-const resetImageForm = () => {
-  imageComment.value = '';
-  imageHashtags.value = '';
-  imageUploadInput.value = '';
-  scaleValue.value = `${SCALE_VALUE_MAX_NUMBER}%`;
+const resetImageForm = (currentForm) => {
+  currentForm.reset();
   changeImageScale(SCALE_VALUE_MAX_NUMBER);
   imagePreview.style.transform = 'scale(1)';
   imagePreview.style.filter = 'none';
@@ -18,4 +12,4 @@ const resetImageForm = () => {
   effectOriginal.checked = true;
 };
 
-export { imageComment, imageHashtags, imageUploadInput, resetImageForm, effectSliderContainer, effectOriginal };
+export { resetImageForm, effectSliderContainer, effectOriginal };
